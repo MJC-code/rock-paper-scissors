adc82c49d2a35adaf0a976b6ae0bcce14a4e3088
Mark C <cookson.mark@gmail.com>
2022-03-18 15:26:00 +1300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function computerPlay() {
+    result = ["Rock", "Paper", "Scissors"];
+    index = Math.floor(Math.random()*3);
+    return result[index];
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+}
